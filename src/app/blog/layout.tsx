f43b806fc64d071b895edad7d3ac7e8ac7daed6f
Neilly28 <neilpilarca@gmail.com>
2023-07-29 18:27:06 +0200

    add initial files

---
+++ src/app/blog/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BlogLayout = ({ children }) => {
+  return (
+    <div>
+      <h1>Hello from Blog Layout</h1>
+      {children}
+    </div>
+  );
+};
+
+export default BlogLayout;
