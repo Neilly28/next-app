d9c980161f34c04066e3064a9ed5616c9ae17711
Neilly28 <neilpilarca@gmail.com>
2023-08-13 16:20:09 +0200

    fixed mobile navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,11 +3,12 @@ import React from "react";
 import Link from "next/link";
 import styles from "./page.module.css";
 import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
+import { BsGithub } from "react-icons/bs";
 
 const links = [
-  { id: 1, title: "Home", url: "/" },
+  // { id: 1, title: "Home", url: "/" },
   { id: 2, title: "Portfolio", url: "/portfolio" },
-  { id: 3, title: "Blog", url: "/blog" },
+  // { id: 3, title: "Blog", url: "/blog" },
   { id: 4, title: "About", url: "/about" },
   { id: 5, title: "Contact", url: "/contact" },
   { id: 6, title: "GitHub", url: "https://github.com/Neilly28" },
@@ -17,21 +18,21 @@ const Navbar = () => {
   return (
     <div className={styles.container}>
       <Link className={styles.logo} href="/">
-        neilapp 😁
+        neily.dev
       </Link>
       <div className={styles.links}>
         <DarkModeToggle />
         {links.map((link) => (
           <Link key={link.id} href={link.url} className={styles.link}>
-            {link.title}
+            {link.title === "GitHub" ? <BsGithub /> : link.title}
           </Link>
         ))}
-        <button
+        {/* <button
           className={styles.logout}
           onClick={() => console.log("loggeout")}
         >
           Logout
-        </button>
+        </button> */}
       </div>
     </div>
   );
