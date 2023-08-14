b5927beff18bcb2173c8c21f890369b6b33dc0d0
Neilly28 <neilpilarca@gmail.com>
2023-08-14 11:13:17 +0200

    removed redundant pages

--- src/app/about/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./page.module.css";
-
-const About = () => {
-  return <div className={styles.container}>About Page</div>;
-};
-
-export default About;
