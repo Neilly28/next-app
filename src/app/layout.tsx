cedc3cb8be6ebf1b6309148ae004ea58bf0c963b
Neilly28 <neilpilarca@gmail.com>
2023-08-14 21:56:29 +0200

    changed arrow size

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
         <link rel="shortcut icon" href="/image/favicon.ico" />
       </head>
       <body className={inter.className}>
